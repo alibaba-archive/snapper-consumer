c6b3b030b3c07686b714bc760f8c889d09e25462
zensh <admin@zensh.com>
2015-06-17 21:25:26 +0800

    support send method

--- index.js
+++ index.js
@@ -3,51 +3,83 @@
 /* global module, define */
 ;(function (root, factory) {
   'use strict'
-  /* istanbul ignore next */
+
   if (typeof module === 'object' && module.exports) module.exports = factory(require('jsonrpc-lite'), require('engine.io-client'))
   else if (typeof define === 'function' && define.amd) define(['jsonrpc-lite', 'engine.io-client'], factory)
-  else root.SocketClient = factory(root.jsonrpc, root.eio)
+  else root.MessageClient = factory(root.jsonrpc, root.eio)
+
 }(typeof window === 'object' ? window : this, function (jsonrpc, Eio) {
   'use strict'
 
   var DELAY = (Math.ceil(Math.random() * 10) + 10) * 1000
 
-  function SocketClient (options) {
-    this.host = options.host
-    this.token = options.token
-    this.path = options.path || '/websocket'
+  function MessageClient (host, options) {
+    this.host = host
+    this.options = options || {}
 
     this.connectDelay = DELAY
+    this.lastRpcId = null
     this.consumerId = null
     this.connection = null
+    this.connected = false
+    this.joinQueue = []
+    this.sendQueue = []
+    this.pending = {}
   }
 
-  SocketClient.prototype.onerror = function (err) {
-    console.error(new Date().toString(), err.stack || err)
+  MessageClient.prototype.onerror = function (err) {
+    console.error(new Date(), err.stack || err)
   }
 
-  SocketClient.prototype.onmessage = function () {}
+  MessageClient.prototype.onmessage = function (message) {
+    console.log(new Date(), message)
+  }
 
-  SocketClient.prototype.joinRoom = function () {}
+  MessageClient.prototype.send = function (method, message, callback) {
+    var packet
+    if (method != null) {
+      packet = new Packet(method, message, callback)
+      this.pending[packet.id] = packet
+      this.sendQueue.push(packet)
+    }
+    if (!this.connected) return this
 
-  SocketClient.prototype.connect = function () {
+    while (this.sendQueue.length) {
+      packet = this.sendQueue.shift()
+      this.connection.send(packet.message)
+    }
+    return this
+  }
+
+  MessageClient.prototype.join = function (room) {
+    if (room != null) this.joinQueue.push(room)
+    if (!this.connected) return this
+    while (this.joinQueue.length) this._join(this.joinQueue.shift(), this.consumerId)
+    return this
+  }
+
+  MessageClient.prototype._join = function (room, consumerId) {
+    throw new Error('not implemented')
+  }
+
+  MessageClient.prototype.connect = function () {
     var ctx = this
-    var query = 'token=' + this.token
-
     if (this.connection) this.connection.off()
 
-    this.connection = new Eio(this.host, {
-      path: this.path,
-      rememberUpgrade: true,
-      query: query
-    })
+    if (this.options.token && !this.options.query) this.options.query = 'token=' + this.token
+    this.connection = new Eio(this.host, this.options)
 
     this.connection
       .on('open', function () {
         ctx.connectDelay = DELAY
         ctx.consumerId = ctx.connection.id
+        this.connected = true
+        ctx.join()
+        ctx.send()
       })
       .on('close', function (err) {
+        this.connected = false
+        this.connection = null
         ctx.onerror(err)
 
         setTimeout(function () {
@@ -61,24 +93,45 @@
       .on('message', function (message) {
         var res = jsonrpc.parse(message)
 
-        if (res.type !== 'request') {
-          return ctx.onerror(new Error('Unprocessable message: ' + JSON.stringify(res)))
+        switch (res.type) {
+          case 'invalid':
+            return ctx.onerror(res.payload)
+
+          case 'notification':
+            return ctx.onmessage(res.payload)
+
+          case 'success':
+          case 'error':
+            if (ctx.pending[res.payload.id]) {
+              ctx.pending[res.payload.id].callback(res.payload.error, res.payload.result)
+              delete ctx.pending[res.payload.id]
+            } else {
+              ctx.onmessage(res.payload)
+            }
+            return
+
+          case 'request':
+            ctx.connection.send(JSON.stringify(jsonrpc.success(res.payload.id, 'OK')))
+            if (res.payload.id === ctx.lastRpcId) return
+            ctx.lastRpcId = res.payload.id
+            ctx.onmessage(res.payload)
         }
-
-        ctx.connection.send(JSON.stringify(jsonrpc.success(res.payload.id, 'OK')))
-
-        while (res.payload.params.length) parserMessage(ctx, res.payload.params.shift())
       })
 
   }
 
-  function parserMessage (client, message) {
-    try {
-      client.onmessage(JSON.parse(message))
-    } catch (error) {
-      client.onerror(error)
-    }
+  function Packet (method, message, callback) {
+    this.id = genRpcId()
+    this.message = JSON.stringify(jsonrpc.request(this.id, method, message))
+    this.callback = callback || noOp
   }
 
-  return SocketClient
+  var count = 0
+  function genRpcId () {
+    return 'MC:' + (++count)
+  }
+
+  function noOp () {}
+
+  return MessageClient
 }))
