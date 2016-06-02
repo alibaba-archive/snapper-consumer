c37bd12bd90214be03f3e82d4782d098aaa23766
Long Yinan <lynweklm@gmail.com>
2016-06-02 22:28:01 +0800

    fix(typings): fix incorrect typings

--- snapper-consumer.d.ts
+++ snapper-consumer.d.ts
@@ -1,22 +1,36 @@
+export interface RequestObject {
+  id: number;
+  jsonrpc: string;
+  method: 'publish' | 'notification' | 'success' | 'error' | 'invalid';
+  params: string[];
+}
+
+export interface RequestEvent {
+  id: number;
+  type: 'request' | 'invalid' | 'notification' | 'success' | 'error';
+  data: RequestObject;
+}
+
 export default class Consumer {
-  constructor(url: string, options: {
+  constructor(url?: string, options?: {
     token: string;
     query: string;
   });
 
   onopen: Function;
   onclose: Function;
-  onerror(err: Error): any;
-  onmessage(event: string): any;
+  onerror(err: Error): void;
+  onmessage(event: RequestEvent): void;
   request(method: string, params: {
     [index: string]: any
   } | any[], callback: (err: Error, res: any) => any): Consumer;
-  join(room: string): Consumer;
+  join(room?: string): Consumer;
   _join(roomId: string, consumerId: string): any;
   _respond(event: string): void;
-  connect(url: string, options: {
+  connect(url: string, options?: {
     token: string;
-    query: string;
+    path?: string;
+    query?: string;
     [index: string]: any;
   }): Consumer;
   close(): void;
